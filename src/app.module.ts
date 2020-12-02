21b54ef0d546327a1ceb2702fb55469336b69c97
dokdo <jcs191072@gmail.com>
2020-12-03 01:54:10 +0900

    make a first entity and connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -29,8 +30,9 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
+      entities: [Restaurant],
     }),
   ],
   controllers: [AppController],
---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,30 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType() // ObjectType for graphql
+@Entity()
+export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
+  @Field(type => String)
+  @Column()
+  name: string;
+
+  @Field(type => Boolean)
+  @Column()
+  isVegan: boolean;
+
+  @Field(type => String)
+  @Column()
+  address: string;
+
+  @Field(type => String)
+  @Column()
+  ownerName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryName: string;
+}
