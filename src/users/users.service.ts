deb8f3b9083339d31a532a087d7cb1f86ed551db
dokdo <jcs191072@gmail.com>
2020-12-04 01:48:54 +0900

    user service: CreateAccount 서비스 로직 구현

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,10 +2,31 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
+    // check new user
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        // make Error
+        return 'There is a user using this email already';
+      }
+      const user = await this.users.create({ email, password, role });
+      await this.users.save(user);
+    } catch (e) {
+      //make error
+      return 'Could not create account';
+    }
+  }
 }
