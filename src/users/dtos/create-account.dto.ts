6b46abfcb33483f800e2c5d7ed362e5559355b80
dokdo <jcs191072@gmail.com>
2020-12-15 23:48:36 +0900

    create login dtos and make common mutation dtos

--- src/users/dtos/create-account.dto.ts
+++ src/users/dtos/create-account.dto.ts
@@ -1,5 +1,6 @@
-import { PickType, InputType, ObjectType, Field } from '@nestjs/graphql';
+import { PickType, InputType, ObjectType } from '@nestjs/graphql';
 import { User } from '../entities/user.entity';
+import { MutationOutput } from 'src/common/dtos/output.dto';
 
 @InputType()
 export class CreateAccountInput extends PickType(User, [
@@ -9,10 +10,4 @@ export class CreateAccountInput extends PickType(User, [
 ]) {}
 
 @ObjectType()
-export class CreateAccountOutput {
-  @Field(type => String, { nullable: true })
-  error?: string;
-
-  @Field(type => Boolean)
-  ok: boolean;
-}
+export class CreateAccountOutput extends MutationOutput {}
---
+++ src/users/dtos/login.dto.ts
@@ -0,0 +1,12 @@
+import { MutationOutput } from 'src/common/dtos/output.dto';
+import { PickType, ObjectType, InputType, Field } from '@nestjs/graphql';
+import { User } from '../entities/user.entity';
+
+@InputType()
+export class LoginInput extends PickType(User, ['email', 'password']) {}
+
+@ObjectType()
+export class LoginOutput extends MutationOutput {
+  @Field(type => String)
+  token: string;
+}
